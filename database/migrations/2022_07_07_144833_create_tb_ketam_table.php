974866ed56b599784532ead4b116fd4eda4613e7
raihanpf22 <raihanpambagyo@.com>
2022-07-07 22:27:30 +0700

    done migration database

---
+++ database/migrations/2022_07_07_144833_create_tb_ketam_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbKetamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_ketam', function (Blueprint $table) {
+            $table->id();
+            $table->string('ulasan');
+            $table->string('rate');
+            $table->string('sentimen');
+            $table->string('datacleaning');
+            $table->string('case_folding');
+            $table->string('tokenize');
+            $table->string('filtering');
+            $table->string('normalisasi');
+            $table->string('stemming');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_ketam');
+    }
+}
---
+++ database/migrations/2022_07_07_145301_create_tb_getas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbGetasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_getas', function (Blueprint $table) {
+            $table->id();
+            $table->string('ulasan');
+            $table->string('rate');
+            $table->string('sentimen');
+            $table->string('datacleaning');
+            $table->string('case_folding');
+            $table->string('tokenize');
+            $table->string('filtering');
+            $table->string('normalisasi');
+            $table->string('stemming');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_getas');
+    }
+}
